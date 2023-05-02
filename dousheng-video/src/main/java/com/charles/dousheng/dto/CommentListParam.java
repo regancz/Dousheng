d070c1fd9ea3b5897a29dcf766839bde6a1b1841
Charles <877477192@qq.com>
2023-05-02 22:20:01 +0800

    update service

---
+++ dousheng-video/src/main/java/com/charles/dousheng/dto/CommentListParam.java
@@ -0,0 +1,21 @@
+package com.charles.dousheng.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author charles
+ * @date 5/2/2023 4:10 PM
+ */
+@Getter
+@Setter
+public class CommentListParam {
+    /**
+     * 用户鉴权token
+     */
+    private String token;
+    /**
+     * 视频id
+     */
+    private Long videoid;
+}
---
+++ dousheng-video/src/main/java/com/charles/dousheng/dto/FavoriteActionParam.java
@@ -0,0 +1,29 @@
+package com.charles.dousheng.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author charles
+ * @date 5/2/2023 3:09 PM
+ */
+@Getter
+@Setter
+public class FavoriteActionParam {
+    /**
+     * 1-点赞，2-取消点赞
+     */
+    @ApiModelProperty("是否点赞")
+    private String actionType;
+    /**
+     * 用户鉴权token
+     */
+    @ApiModelProperty("用户鉴权token")
+    private String token;
+    /**
+     * 视频id
+     */
+    @ApiModelProperty("视频id")
+    private Long videoid;
+}
