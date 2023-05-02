28919aa02e81b4afb930b32820feb47994bad0b6
Charles <877477192@qq.com>
2023-05-02 22:35:45 +0800

    update relation

--- dousheng-video/src/main/java/com/charles/dousheng/service/RelationUserService.java
+++ dousheng-video/src/main/java/com/charles/dousheng/service/RelationUserService.java
@@ -1,8 +1,20 @@
 package com.charles.dousheng.service;
 
+import com.charles.dousheng.dto.RelationActionParam;
+import com.charles.dousheng.dto.RelationFollowListParam;
+import com.charles.dousheng.dto.RelationFollowerListParam;
+import com.charles.dousheng.dto.UserResult;
+
+import java.util.List;
+
 /**
  * @author charles
  * @date 5/1/2023 10:10 PM
  */
 public interface RelationUserService {
+    List<UserResult> relationFollowList(RelationFollowListParam relationFollowListParam);
+
+    List<UserResult> relationFollowerList(RelationFollowerListParam relationFollowerListParam);
+
+    int relationAction(RelationActionParam relationActionParam);
 }
--- dousheng-video/src/main/java/com/charles/dousheng/service/impl/RelationUserUserServiceImpl.java
+++ dousheng-video/src/main/java/com/charles/dousheng/service/impl/RelationUserUserServiceImpl.java
@@ -1,7 +1,13 @@
 package com.charles.dousheng.service.impl;
 
+import com.charles.dousheng.dto.RelationActionParam;
+import com.charles.dousheng.dto.RelationFollowListParam;
+import com.charles.dousheng.dto.RelationFollowerListParam;
+import com.charles.dousheng.dto.UserResult;
 import com.charles.dousheng.service.RelationUserService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author charles
@@ -9,4 +15,18 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RelationUserUserServiceImpl implements RelationUserService {
+    @Override
+    public List<UserResult> relationFollowList(RelationFollowListParam relationFollowListParam) {
+        return null;
+    }
+
+    @Override
+    public List<UserResult> relationFollowerList(RelationFollowerListParam relationFollowerListParam) {
+        return null;
+    }
+
+    @Override
+    public int relationAction(RelationActionParam relationActionParam) {
+        return 0;
+    }
 }
