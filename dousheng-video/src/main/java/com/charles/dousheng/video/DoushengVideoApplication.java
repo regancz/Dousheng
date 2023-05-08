89e2757f1f52561a6812e5a4cee4ccbb8f2a69bd
Charles <877477192@qq.com>
2023-05-08 22:43:20 +0800

    support xxljob

--- dousheng-video/src/main/java/com/charles/dousheng/video/DoushengVideoApplication.java
+++ dousheng-video/src/main/java/com/charles/dousheng/video/DoushengVideoApplication.java
@@ -3,6 +3,7 @@ package com.charles.dousheng.video;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author charles
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class DoushengVideoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DoushengVideoApplication.class, args);
